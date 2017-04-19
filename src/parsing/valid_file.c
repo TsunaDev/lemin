dc20f90640a60e8bd3eed7efcb8a6af0c603d816
irakiz_g <glenn-gabriel.irakiza@epitech.eu>
2017-04-19 19:31:16 +0000

    norme :(

--- src/parsing/valid_file.c
+++ src/parsing/valid_file.c
@@ -5,7 +5,7 @@
 ** Login   <glenn-gabriel.irakiza@epitech.eu>
 **
 ** Started on  Tue Apr 18 08:49:42 2017 Glenn Gabriel Irakiza
-** Last update Wed Apr 19 20:40:58 2017 Anaïs Breant
+** Last update Wed Apr 19 18:35:25 2017 Glenn Gabriel Irakiza
 */
 
 #include	"check_file.h"
@@ -33,55 +33,9 @@ static int	my_arg_valid(char *str)
   return (0);
 }
 
-static int	my_connect_valid(char *str)
-{
-  char		**connect;
-  int		isnum;
-  int		y;
-
-  y = 0;
-  connect = my_strnchar_to_wordtab(str, '-');
-  while (connect[y] != NULL)
-    {
-      isnum = my_isnum(connect[y]);
-      if (isnum != 0)
-	{
-	  my_free_array(connect);
-	  return (1);
-	}
-      y++;
-    }
-  my_free_array(connect);
-  return (0);
-}
-
-static int	pos_error_connect_room(char **arr)
-{
-  int		y;
-  int		err;
-  int		nb_words;
-
-  y = 0;
-  err = 0;
-  while (arr[y] != NULL)
-    {
-      nb_words = my_nb_words_char(arr[y], '-', 0);
-      if (nb_words == 2)
-	{
-	  err = my_connect_valid(arr[y]);
-	  if (err == 1)
-	    return (y);
-	}
-      nb_words = my_nb_words_char(arr[y], ' ', 0);
-      y++;
-    }
-  return (-1);
-}
-
 static int	pos_error_room(char **arr)
 {
   int		nb_words;
-  int		rt;
   int		err;
   int		y;
 
@@ -96,7 +50,6 @@ static int	pos_error_room(char **arr)
 	  if (err == 1)
 	    return (y);
 	}
-      nb_words = my_nb_words_char(arr[y], '-', 0);
       y++;
     }
   return (-1);
