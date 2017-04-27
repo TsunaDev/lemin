87def942464d3c37012cfc4b7fef9c26ff934ecd
irakiz_g <glenn-gabriel.irakiza@epitech.eu>
2017-04-27 13:20:22 +0000

    check file

--- src/parsing/check_file.c
+++ src/parsing/check_file.c
@@ -5,12 +5,44 @@
 ** Login   <anais.breant@epitech.eu>
 **
 ** Started on  Wed Apr 12 19:34:56 2017 Anaïs Breant
-** Last update Thu Apr 27 14:43:20 2017 Anaïs Breant
+** Last update Thu Apr 27 13:18:46 2017 Glenn Gabriel Irakiza
 */
 
 #include	<unistd.h>
 #include	"check_file.h"
 #include	"my_string.h"
+
+static int	my_change_arr(char **arr)
+{
+  int		y;
+  int		isnum;
+  int		i;
+
+  y = 0;
+  i = 0;
+  if (arr[y] == NULL)
+    return (84);
+  isnum = my_isnum(arr[y]);
+  if (isnum != 0 && (arr[y][0] == '#' && arr[y][1] != '#'))
+    {
+      while (isnum == 1)
+	{
+	  y++;
+	  isnum = my_isnum(arr[y]);
+	  if (isnum != 0 && (arr[y][0] == '#' && arr[y][1] != '#'))
+	    free(arr[y]);
+	  else
+	    isnum =0;
+	}
+    }
+  while (arr[y] != NULL)
+    {
+      arr[i] = arr[y];
+      i++;
+      y++;
+    }
+  return (0);
+}
 
 int		check_file(char **arr)
 {
@@ -19,6 +51,7 @@ int		check_file(char **arr)
 
   if (arr == NULL)
     return (-1);
+  my_change_arr(arr);
   return_value = check_nbr_ants(arr[0]);
   if (return_value == -1)
     {
