4822ce56f6efa14607be0ebab5a1b56d7f64d7c0
LaFleche <vincent.larcher@epitech.eu>
2017-04-30 20:32:31 +0200

    merge my_parse bonus

--- bonus/src/parsing/my_pars.c
+++ bonus/src/parsing/my_pars.c
@@ -5,7 +5,7 @@
 ** Login   <glenn-gabriel.irakiza@epitech.eu>
 **
 ** Started on  Mon Apr 10 14:28:50 2017 Glenn Gabriel Irakiza
-** Last update Sun Apr 30 20:04:25 2017 Martin Van Elslande
+** Last update Sun Apr 30 20:28:48 2017 LaFleche
 */
 
 #include	"stock_tun.h"
@@ -13,6 +13,8 @@
 #include	"check_file.h"
 #include	"my_string.h"
 #include	"pathfinding.h"
+
+
 
 static int	nb_of_rooms(t_room **room)
 {
@@ -24,15 +26,46 @@ static int	nb_of_rooms(t_room **room)
   return (i);
 }
 
-int		my_pars(char **arr)
+static void     my_display_arr(char **arr, t_room **room)
 {
-  int		nbr_ants;
-  t_room	**room;
-  int		**tab_tun;
-  int		status_room;
-  int		size;
-  int		**paths;
-  int		ret;
+  arr = check_nbr_room(room, arr);
+  display_arr(arr);
+  suppr_double_tun(arr);
+}
+
+static int      my_path_tun(t_room **room, int nbr_ants, char **arr)
+{
+  int           **paths;
+  int           **tab_tun;
+  int           nb_room;
+  int           ret;
+
+  nb_room = nb_of_rooms(room);
+  tab_tun = create_tab_int(room, arr);
+  ret = pathfinding(tab_tun, tab_tun[0][0], nb_room, &paths);
+  if (ret == 0)
+    display_graphical(nbr_ants, room, tab_tun, paths);
+  return (ret);
+}
+
+static int      create_rooom(t_room **room, char **arr)
+{
+  int           ret;
+
+  ret = 0;
+  my_create_room(room, arr, &ret);
+  if (ret == 84)
+    return (84);
+  my_sort_room(room);
+  return (ret);
+}
+
+int             my_pars(char **arr)
+{
+  t_room        **room;
+  int           nbr_ants;
+  int           size;
+  int           ret;
 
   if (arr == NULL)
     return (84);
@@ -45,17 +78,10 @@ int		my_pars(char **arr)
   room = malloc(sizeof(t_room *) * size);
   if (room == NULL || arr == NULL)
     return (84);
-  status_room = 0;
-  my_create_room(room, arr, &status_room);
-  if (status_room == 84)
+  ret = create_rooom(room, arr);
+  if (ret == 84)
     return (84);
-  my_sort_room(room);
-  arr = check_nbr_room(room, arr);
-  display_arr(arr);
-  suppr_double_tun(arr);
-  tab_tun = create_tab_int(room, arr);
-  ret = pathfinding(tab_tun, tab_tun[0][0], nb_of_rooms(room), &paths);
-  if (ret == 0)
-    display_graphical(nbr_ants, room, tab_tun, paths);
+  my_display_arr(arr, room);
+  ret = my_path_tun(room, nbr_ants, arr);
   return (ret);
 }
